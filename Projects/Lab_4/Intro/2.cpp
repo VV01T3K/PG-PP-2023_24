7dea316f43a614bbf2791643243a30c6d9ff6a59
VV01T3K <wojteks.access@gmail.com>
2023-10-30 15:13:50 +0000

    intro_lab4

--- Projects/Lab_4/Intro/2.cpp
+++ Projects/Lab_4/Intro/2.cpp
@@ -3,23 +3,50 @@
 // from 33 till 126 ASCII
 // https://www.asciitable.com/
 
-#define valid_char(x)                                   \
-    (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || \
-        (x >= '0' && x <= '9') || (x == '_')
+#define wSpace(x) (x == ' ' || x == '\n' || x == '\t')
+
+#define valid_char(x)                                    \
+    ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || \
+     (x >= '0' && x <= '9') || (x == '_'))
+
+#define valid_first_char(x) \
+    ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || (x == '_'))
 
 int main() {
-    char c;
+    int c;
+
     int words = 0;
-    int word_state = 0;
+    int word_state = 1;
+    int valid = 1;
+    int first_char = 1;
 
     while ((c = fgetc(stdin)) != EOF) {
-        if (c == 'w') {
-            word_state = 1;
-        } else {
+        if (wSpace(c)) {
+            if (word_state == 1) {
+                if (valid == 1) {
+                    words++;
+                }
+                valid = 1;
+                first_char = 1;
+            }
             word_state = 0;
+        } else {
+            word_state = 1;
+            if (first_char) {
+                first_char = 0;
+                if (!valid_first_char(c)) {
+                    valid = 0;
+                }
+            }
+
+            if (!valid_char(c)) {
+                valid = 0;
+            }
+        }
+        if (c == '\n') {
+            printf("%d\n", words);
+            words = 0;
         }
     }
-    printf("%d\n", words);
-
     return 0;
 }
