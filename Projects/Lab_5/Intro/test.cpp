4fab6208a13f954847eab952afb5d0b0e58570d8
VV01T3K <wojteks.access@gmail.com>
2023-11-01 15:53:40 +0000

    worse alternative

--- Projects/Lab_5/Intro/test.cpp
+++ Projects/Lab_5/Intro/test.cpp
@@ -1,38 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-#define listS 500
-#define wordS 1000
+void strrev(char* str) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
 
 int main() {
-    char c;
-    char word_list[listS][wordS] = {};
-    char exist_list[listS] = {0};
-    int list_len = 0;
-    char array[200][200] = {0};
-    int row = 0, col, k;
-    char word[wordS] = {0};
-    // pobranie listy słów
-    do {
-        scanf("%s%c", word_list[list_len], &c);
-        list_len++;
-    } while (c != '\n');
+    char str[] = "Hello world";  // initializing a char array
 
-    // TODO - dodać opcje różno wielkościowych arrayów
-    // pobranie arraya liter
-    // for (row = 0, col = 0; row < rowS;
-    //      col++, (col == colS) ? col = 0, row++ : 0) {
-    //     scanf("%c", &c);
-    //     if (c == '\n') scanf("%c", &c);
-    //     array[row][col] = c;
-    // }
-    int rowS = 0, colS = 0;
+    printf("The string is : %s\n", str);  // printing the actual array
 
-    while (scanf("%s", word) == 1) {
-        rowS++;
-    }
-    colS = strlen(word);
+    strrev(str);  // reversing the char array
 
-    printf("%d %d\n", rowS, colS);
+    printf("The string after using function strrev() is : %s\n",
+           str);  // printing the reversed array
     return 0;
 }
