aab3d58eca150cbbaa68dcf029df4bb42db601bf
VV01T3K <wojteks.access@gmail.com>
2023-11-21 22:57:18 +0000

    padding

--- Project/index.c
+++ Project/index.c
@@ -26,25 +26,33 @@ void paint_DWOR(int x, int y, WINDOW* window) {
     }
 }
 
-void paint_BOARD(WINDOW* window) {
+WINDOW* paint_BOARD(int pad) {
+    WINDOW* window = newwin(13 + (pad * 2), 51 + (pad * 4), 0, 0);
+    refresh();
+    box(window, 0, 0);
     int i;
     for (i = 0; i < 14; i++) {
         if (i == 6) {
-            paint_BAR(1 + 3 * i++, 1, window);
+            paint_BAR(pad * 2 + 1 + 3 * i++, 1 + pad, window);
             continue;
         }
-        paint_POLE(3 * i + 2, 1, (i % 2 ? RED : YELLOW), POLE_DOWN, window);
-        paint_POLE(3 * i + 2, 7, (i % 2 ? YELLOW : RED), POLE_UP, window);
+        paint_POLE(3 * i + 2 + pad * 2, 1 + pad, (i % 2 ? RED : YELLOW),
+                   POLE_DOWN, window);
+        paint_POLE(3 * i + 2 + pad * 2, 7 + pad, (i % 2 ? YELLOW : RED),
+                   POLE_UP, window);
     }
-    paint_DWOR(3 * i + 1, 1, window);
+    paint_DWOR(3 * i + +1 + pad * 2, pad + 1, window);
+    // mvwprintw(window, 0, 1, "Backgammon 1.0");
+    wrefresh(window);
+    return window;
 }
 
 void run() {
     // WINDOW* BOARD = newwin(13, 50, 1, 4);
-    WINDOW* BOARD = newwin(13, 50, 0, 0);
-    refresh();
-    box(BOARD, 0, 0);
-    paint_BOARD(BOARD);
+    const int padding = 1;
+    WINDOW* BOARD = paint_BOARD(padding);
+
+    mvwprintw(BOARD, 0, 1, "Backgammon-1.0");
     wrefresh(BOARD);
 
     refresh();
