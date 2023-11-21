60bed499a82509fa777f94342b793778e622d9c3
VV01T3K <wojteks.access@gmail.com>
2023-11-21 16:58:43 +0000

    working init

--- Project/index.c
+++ Project/index.c
@@ -1,8 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <ncurses.h>
 
 int main() {
-    // code
-    printf("Hello World!\n");
+    initscr();                 // Start
+    if (has_colors() == TRUE)  // 1
+    {
+        start_color();                         // 2
+        init_pair(1, COLOR_RED, COLOR_BLACK);  // 3
+        attron(COLOR_PAIR(1));                 // 4
+        printw("Kolorowy napis. ;P");
+        attroff(COLOR_PAIR(1));  // Wyłączenie koloru tekstu
+    } else {
+        printw("Twoja Konsolka nie obsluguje kolorow. :/ ");
+    }
+    getch();
+    endwin();  // koniec
     return 0;
 }
